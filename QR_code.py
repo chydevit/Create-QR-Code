5b7b9b86f5c60467b541842bee226780acbd28cb
new-username <new-email@example.com>
2026-01-06 16:16:32 +0700

    add ui

--- QR_code.py
+++ QR_code.py
@@ -1,9 +1,9 @@
-# import qrcode
-# #this for add url or text
-# data = 'https://www.tiktok.com/en/'
+import qrcode
+#this for add url or text
+data = 'hi'
 
-# # this function create qr code
-# img = qrcode.make(data)
+# this function create qr code
+img = qrcode.make(data)
 
-# #this for add name photo
-# img.save('QR_code_TIKTOK.jpg')
+#this for add name photo
+img.save('po.jpg')
